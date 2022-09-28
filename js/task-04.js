23d629e95244f0de339b163a15290a0a8497a786
nonbinary00 <agarkovaalysh12@gmail.com>
2022-09-28 16:09:14 +0200

    task 4 fix
    Create a counterValue variable that will store the current value of the counter and initialize it to 0.
    Add click listeners to buttons, inside which you can increase or decrease the counter value.
    Update the interface with the new value of the counterValue variable.
    
    Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
    Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
    Обновляй интерфейс новым значением переменной counterValue.

--- js/task-04.js
+++ js/task-04.js
@@ -1,19 +1,35 @@
-const result = document.querySelector("#value");
+// const result = document.querySelector("#value");
 
-const counterValue = 0;
-const buttonPlus = document.querySelector('[data-action="increment"]');
-const buttonMinus = document.querySelector('[data-action="decrement"]');
+// const counterValue = 0;
+// const buttonPlus = document.querySelector('[data-action="increment"]');
+// const buttonMinus = document.querySelector('[data-action="decrement"]');
 
-const clickPlus = () => {
-	counterValue += 1;
-	result.textContent = counterValue;
-};
+// const clickPlus = () => {
+// 	counterValue += 1;
+// 	result.textContent = counterValue;
+// };
 
 
-const clickMinus = () => {
-	counterValue -= 1;
-	result.textContent = counterValue;
-};
+// const clickMinus = () => {
+// 	counterValue -= 1;
+// 	result.textContent = counterValue;
+// };
 
 // buttonPlus.addEventListener("click", clickPlus);
 // buttonMinus.addEventListener("click", clickMinus);
+
+let counterValue = 0;
+
+const decrement = document.querySelector('[data-action="decrement"]');
+const increment = document.querySelector('[data-action="increment"]');
+const counter = document.querySelector("#value")
+
+decrement.addEventListener("click", () => {
+  counterValue -= 1;
+  counter.textContent = counterValue;
+});
+
+increment.addEventListener("click", () => {
+  counterValue += 1;
+  counter.textContent = counterValue;
+});
