d9b1a3f9474fc1ad6fe4d865d167086bcf969f70
nonbinary00 <agarkovaalysh12@gmail.com>
2022-09-30 23:06:43 +0200

    task 9
    Form.login-form submission handling should be on the submit event.
    When submitting the form, the page should not reload.
    If there are blank fields in the form, display an alert warning that all fields must be filled.
    If the user has filled in all the fields and submitted the form, collect the field values into an object where the field name will be the property name and the field value will be the property value. To access form elements, use the elements property.
    Display the object with the entered data in the console and clear the values of the form fields using the reset method.
    
    Обработка отправки формы form.login-form должна быть по событию submit.
    При отправке формы страница не должна перезагружаться.
    Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
    Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
    Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", event => {
+	event.preventDefault();
+	const { email, password } = loginForm.elements;
+	if (email.value === "" || password.value === "") {
+		return alert("Усі поля повинні бути заповнені");
+	}
+	console.log(`Email: ${email.value}, 
+            Password: ${password.value}`);
+	loginForm.reset();
+});
