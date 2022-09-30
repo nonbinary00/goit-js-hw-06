d2ccf5342acb18ad32657c1625e1a2846fbbbdc9
nonbinary00 <agarkovaalysh12@gmail.com>
2022-09-30 23:08:38 +0200

    task 9
    Write a script that changes the background colors of the <body> element via inline style when button.change-color is clicked and outputs the color value to span.color.
    
    Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+const changeColor = document.querySelector(".change-color");
+
+changeColor.addEventListener("click", () => {
+	const color = getRandomHexColor();
+	body.style.backgroundColor = color;
+	spanColor.textContent = color;
+});
